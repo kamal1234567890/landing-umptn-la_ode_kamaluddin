dc4a6acc180b0462a0dceaa7b677ca52a068750d
kamal1234567890 <kamaluddin1623@gmail.com>
2022-10-27 16:58:51 +0800

    commit pertama

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ['./public/**/*.{html,js}'],
+  content: ['./index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
